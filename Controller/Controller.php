304f10f7458c5f993e083e34032e0dd5baab8c04
Alexandre Kilian <alexandre@diamonddev.com>
2016-01-20 10:21:03 +0100

    ADD: Response types + Factory, Base Controller, services && Form Type entity hidden

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -3,10 +3,29 @@
 namespace Pulpmedia\NgHttpBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Pulpmedia\NgHttpBundle\HttpFoundation\ErrorResponse;
+use Pulpmedia\NgHttpBundle\HttpFoundation\SuccessResponse;
 
 class Controller extends Controller
 {
-    public function renderResponse($data, $status = 200, $headers = array()){
+    public function success($data, $headers = array(), $status = 200){
+        $response = $this->get('pulpmedia_ng_http.response.factory')->getSuccessResponse();
+        $response->setContent($data);
+        $response->setStatusCode($status);
+
+        return $response;
 
     }
+    public function error($errorCode, $errors = array(), $data = null, $status = 404){
+
+        $response = $this->get('pulpmedia_ng_http.response.factory')->getErrorResponse();
+        $response->setErrorCode($errorCode);
+        $response->setErrors($errors);
+        $response->setContent($data);
+        $response->setStatusCode($status);
+
+        return $response;
+    }
+
 }

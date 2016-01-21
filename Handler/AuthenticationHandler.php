7233f99ff36f0f48b505b7ac6274ac1625ae5487
Alexandre Kilian <alexandre@diamonddev.com>
2016-01-21 08:27:46 +0100

    add missing file

---
+++ Handler/AuthenticationHandler.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Pulpmedia\NgHttpBundle\Handler;
+
+use Symfony\Component\HttpFoundation\Response;
+use Pulpmedia\NgHttpBundle\Component\HttpFoundation\SuccessResponse;
+use Pulpmedia\NgHttpBundle\Component\HttpFoundation\ErrorResponse;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
+use Pulpmedia\NgHttpBundle\Services\ResponseFactory;
+
+class AuthenticationHandler implements AuthenticationSuccessHandlerInterface, AuthenticationFailureHandlerInterface
+{
+
+    private $rf;
+    public function __construct(ResponseFactory $rf){
+        $this->rf = $rf;
+    }
+
+    /**
+     * This is called when an interactive authentication attempt succeeds. This
+     * is called by authentication listeners inheriting from
+     * AbstractAuthenticationListener.
+     *
+     * @see \Symfony\Component\Security\Http\Firewall\AbstractAuthenticationListener
+     * @param Request        $request
+     * @param TokenInterface $token
+     * @return Response the response to return
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+            $result = array('success' => true);
+            $response = $this->rf->getSuccessResponse();
+            $response->setContent($result);
+            return $response;
+    }
+
+    /**
+     * This is called when an interactive authentication attempt fails. This is
+     * called by authentication listeners inheriting from
+     * AbstractAuthenticationListener.
+     *
+     * @param Request                 $request
+     * @param AuthenticationException $exception
+     * @return Response the response to return
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+
+            $response = $this->rf->getErrorResponse();
+            $response->setErrors(array('message' => $exception->getMessage()));
+            return $response;
+    }
+}
